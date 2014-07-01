f6c5f47fe420c3781a32ff14899ac62cf1485d29
anqing <490406033@qq.com>
2014-07-01 10:07:13 +0800

    first commit: add all files

---
+++ GitHub_demo_Project/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitHub_demo_Project
+//
+//  Created by 我去 on 14-7-1.
+//  Copyright (c) 2014年 an qing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitHub_demo_Project/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitHub_demo_Project
+//
+//  Created by 我去 on 14-7-1.
+//  Copyright (c) 2014年 an qing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
