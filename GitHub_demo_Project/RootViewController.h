201f6b6a8ea4233784c289b4e517b8455128837d
anqing <490406033@qq.com>
2014-07-01 10:36:11 +0800

    add rootViewController

---
+++ GitHub_demo_Project/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  GitHub_demo_Project
+//
+//  Created by 我去 on 14-7-1.
+//  Copyright (c) 2014年 an qing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ GitHub_demo_Project/RootViewController.m
@@ -0,0 +1,43 @@
+//
+//  RootViewController.m
+//  GitHub_demo_Project
+//
+//  Created by 我去 on 14-7-1.
+//  Copyright (c) 2014年 an qing. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+
+
+
+@interface RootViewController ()
+
+@end
+
+
+
+@implementation RootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
