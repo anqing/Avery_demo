1d45735c95765d3aebbb51407feccbf4e7f0cff4
anqing <490406033@qq.com>
2014-07-01 10:39:13 +0800

    set the rootview of window

--- GitHub_demo_Project/AppDelegate.m
+++ GitHub_demo_Project/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "RootViewController.h"
 
 @implementation AppDelegate
 
@@ -15,6 +16,15 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    
+    
+    RootViewController *rootVC = [[RootViewController alloc] init];
+    self.window.rootViewController = rootVC;
+    [rootVC release];
+    
+    
+    
     [self.window makeKeyAndVisible];
     return YES;
 }
